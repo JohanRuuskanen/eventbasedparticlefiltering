e09ce5536fa784bf587da1ab31d5f9c1fd60e100
Johan Ruuskanen <johan.ruuskanen@gmail.com>
2018-10-09 22:00:31 +0200

    Update nonlinear estimation

--- test_nonlinear/examine_system.jl
+++ test_nonlinear/examine_system.jl
@@ -7,20 +7,15 @@ include("filters_eventbased.jl")
 include("../src/misc.jl")
 
 T = 200
-N = 100
+N = 200
 
 δ = 5
 
-Q = 1.0
-R = 0.1
-
-w = Normal(0.0, 1)#Gumbel(0, 1)
+w = Normal(0.0, 1)
 v = Normal(0.0, 0.1)
 
-wh = Normal(mean(w), std(w)*sqrt(2))
-vh = Normal(mean(v), std(v)*sqrt(2))
 
-f(x, k) = x/2 + 25*x./(1 + x.^2) + 8*cos(0.5*k)
+f(x, k) = x/2 + 25*x./(1 + x.^2) + 8*cos(1.2*k)
 h(x, k) = x.^2/20
 
 sys = sys_params(f, h, w, v, T, [1, 1])
@@ -28,46 +23,76 @@ par = pf_params(N)
 
 x, y = sim_sys(sys)
 
-X1, W1 = bpf(y, sys, N)
-X2, W2 = apf(y, sys, par)
-X3, W3, Z3, Γ3 = ebpf(y, sys, par, δ)
-X4, W4, Z4, Γ4 = eapf(y, sys, par, δ)
+tic()
+X1, W1, Z1, Γ1, Neff1, res1, fail1 = ebpf(y, sys, par, δ)
+t1 = toc()
 
-
+tic()
+X2, W2, Z2, Γ2, Neff2, res2, fail2 = eapf(y, sys, par, δ)
+t2 = toc()
 
 xh1 = sum(diag(W1'*X1[:, 1, :]), 2)
 xh2 = sum(diag(W2'*X2[:, 1, :]), 2)
-xh3 = sum(diag(W3'*X3[:, 1, :]), 2)
-xh4 = sum(diag(W4'*X4[:, 1, :]), 2)
 
+err1 = x' - xh1
+err2 = x' - xh2
+
+idx1 = find(x -> x == 1, Γ1)
+idx2 = find(x -> x == 1, Γ2)
+
+err1_t = err1[idx1]
+err2_t = err2[idx2]
+
+
+
+println("")
+println("Total error")
+println("EBPF x: $(mean(err1.^2))")
+println("EAPF x: $(mean(err2.^2))")
+println("")
+println("Error at new measurements")
+println("EBPF x: $(mean(err1_t.^2))")
+println("EAPF x: $(mean(err2_t.^2))")
+println("")
+println("EBPF t: $(t1)")
+println("EAPF t: $(t2)")
+println("")
+println("Special")
+println("EBPF Neff: $(mean(Neff1))")
+println("EBPF res: $(sum(res1))")
+println("EBPF fail: $(sum(fail1))")
+println("")
+println("EAPF Neff: $(mean(Neff2))")
+println("EAPF res: $(sum(res2))")
+println("EAPF fail: $(sum(fail2))")
+println("")
 
 figure(1)
 clf()
 subplot(2, 1, 1)
-plot(1:T, y')
+plot(y')
+plot(Z1')
+plot(Z2')
+legend(["y", "z BPF", "z APF"])
 subplot(2, 1, 2)
-plot(1:T, x')
-plot(1:T, xh1)
-plot(1:T, xh2)
-legend(["True", "BPF", "APF"])
+plot(x')
+plot(xh1)
+plot(xh2)
+legend(["True", "EBPF", "EAPF"])
 
+idx_res1 = find(x->x == 1, res1)
+idx_fail1 = find(x->x == 1, fail1)
+idx_res2 = find(x->x == 1, res2)
+idx_fail2 = find(x->x == 1, fail2)
 figure(2)
 clf()
+title("Effective sample size")
 subplot(2, 1, 1)
-plot(1:T, y')
-plot(1:T, Z3')
+plot(1:T, Neff1, "C0")
+plot((1:T)[idx_res1], Neff1[idx_res1], "C0o")
+plot((1:T)[idx_fail1], Neff1[idx_fail1], "C0x")
 subplot(2, 1, 2)
-plot(1:T, x')
-plot(1:T, xh3)
-plot(1:T, xh4)
-legend(["True", "BPF"])
+plot(1:T, Neff2, "C0")
+plot((1:T)[idx_res2], Neff2[idx_res2], "C0o")
+plot((1:T)[idx_fail2], Neff2[idx_fail2], "C0x")
 
-x = linspace(-10, 10, 1000)
-figure(3)
-clf()
-subplot(2, 1, 1)
-plot(x, pdf.(w, x))
-plot(x, pdf.(wh, x))
-subplot(2, 1, 2)
-plot(x, pdf.(v, x))
-plot(x, pdf.(vh, x))
--- test_nonlinear/filters_eventbased.jl
+++ test_nonlinear/filters_eventbased.jl
@@ -1,5 +1,5 @@
 """
-Event driven particle filters using the MBT kernel
+Event driven particle filters
 """
 function ebpf(y, sys, par, δ)
     """
@@ -26,8 +26,15 @@ function ebpf(y, sys, par, δ)
     X[:, :, 1] = rand(Normal(0, 10), N, nx)
     W[:, 1] = 1/N .* ones(N, 1)
 
+    Neff = zeros(T)
+    res = zeros(T)
+    fail = zeros(T)
+
+    N_T = N / 2
+
     idx = collect(1:N)
     Xr = X[:, 1]
+    Wr = W[:, 1]
 
     for k = 2:T
 
@@ -40,21 +47,26 @@ function ebpf(y, sys, par, δ)
             Z[:, k] = Z[:, k-1]
         end
 
-        # Resample using systematic resampling
-        idx = collect(1:N)
-        wc = cumsum(W[:, k-1])
-        u = (([0:(N-1)] + rand()) / N)[1]
-        c = 1
-        for i = 1:N
-            while wc[c] < u[i]
-                c = c + 1
+        Neff[k] = 1/sum(W[:, k-1].^2)
+        if Neff[k] < N_T
+            # Resample using systematic resampling
+            idx = collect(1:N)
+            wc = cumsum(W[:, k-1])
+            u = (([0:(N-1)] + rand()) / N)[1]
+            c = 1
+            for i = 1:N
+                while wc[c] < u[i]
+                    c = c + 1
+                end
+                idx[i] = c
             end
-            idx[i] = c
-        end
-
-        # Resample
-        for i = 1:N
-            Xr[i, :] = X[idx[i], :, k-1]
+            
+            Xr = X[idx, :, k-1]
+            Wr = 1/N .* ones(N, 1)
+            res[k] = 1
+        else
+            Xr = X[:, :, k-1]
+            Wr = W[:, k-1]
         end
 
         # Propagate
@@ -65,12 +77,12 @@ function ebpf(y, sys, par, δ)
         # Weight
         if Γ[k] == 1
             for i = 1:N
-                W[i, k] = pdf.(sys.v, y[k] - h(X[i, :, k], k)[1])
+                W[i, k] = Wr[i]*pdf.(sys.v, y[k] - h(X[i, :, k], k)[1])
             end
         elseif Γ[k] == 0
             for i = 1:N
-                D = Normal(h(X[i, :, k], k)[1], std(w))
-                W[i, k] = cdf(D, Z[k] + δ) - cdf(D, Z[k] - δ)
+                D = Normal(h(X[i, :, k], k)[1], std(v))
+                W[i, k] = Wr[i]*cdf(D, Z[k] + δ) - cdf(D, Z[k] - δ)
             end
         end
 
@@ -79,10 +91,12 @@ function ebpf(y, sys, par, δ)
         else
             println("Warning BPF: restting weights to uniform")
             W[:, k] = 1/N .* ones(N, 1)
+            fail[k] = 1
+            Neff[k] = 0
         end
 
     end
-    return X, W, Z, Γ
+    return X, W, Z, Γ, Neff, res, fail
 end
 
 function eapf(y, sys, par, δ)
@@ -91,12 +105,11 @@ function eapf(y, sys, par, δ)
     """
     f = sys.f
     h = sys.h
+    w = sys.w
+    v = sys.v
 
-    Q = std(sys.w)
-    R = std(sys.v)
-
-    Qh = std(sys.w)*2
-    Rh = std(sys.v)*2
+    Q = var(w)
+    R = var(v)
 
     nx = sys.nd[1]
     ny = sys.nd[2]
@@ -118,7 +131,13 @@ function eapf(y, sys, par, δ)
 	q_aux_list = Array{Distribution}(par.N)
 
 	Xr = X[:, :, 1]
+    Wr = W[:, 1]
 
+    Neff = zeros(T)
+    res = zeros(T)
+    fail = zeros(T)
+
+    N_T = N/2
     # === For approximating the uniform distribution
     # number of approximation points and their spacing
     M = 20 #ceil(2 * δ) + 1
@@ -153,7 +172,7 @@ function eapf(y, sys, par, δ)
                 μ = JP_mu(X[i, :, k-1], k)
                 Σ = JP_sig(X[i, :, k-1], k, R)
 
-        	    q_aux_list[i] = MvNormal(μ[2], Σ[2,2])
+                q_aux_list[i] = MvNormal(μ[2], sqrt(Σ[2,2]))
         	    V[i, k-1] = W[i, k-1] * pdf(q_aux_list[i], y[:,k])
         	end
             V[:, k-1] = V[:, k-1] ./ sum(V[:, k-1])
@@ -162,7 +181,7 @@ function eapf(y, sys, par, δ)
                 μ = JP_mu(X[i, :, k-1], k)
                 Σ = JP_sig(X[i, :, k-1], k, R + Vn)
 
-                q_aux_list[i] = MvNormal(μ[2], Σ[2,2])
+                q_aux_list[i] = MvNormal(μ[2], sqrt(Σ[2,2]))
                 predLh = 0
                 for j = 1:M
                     predLh += pdf(q_aux_list[i], yh[j, :])
@@ -172,23 +191,29 @@ function eapf(y, sys, par, δ)
         end
 	    V[:, k-1] = V[:, k-1] ./ sum(V[:, k-1])
 
-        # Resample using systematic resampling
-        idx = collect(1:N)
-        wc = cumsum(V[:, k-1])
-        u = (([0:(N-1)] + rand()) / N)[1]
-        c = 1
-        for i = 1:N
-            while wc[c] < u[i]
-                c = c + 1
+        Neff[k] = 1/sum(V[:, k-1].^2)
+        if Neff[k] < N_T 
+            # Resample using systematic resampling
+            idx = collect(1:N)
+            wc = cumsum(V[:, k-1])
+            u = (([0:(N-1)] + rand()) / N)[1]
+            c = 1
+            for i = 1:N
+                while wc[c] < u[i]
+                    c = c + 1
+                end
+                idx[i] = c
             end
-            idx[i] = c
+
+            Xr = X[idx, :, k-1]
+            Wr = 1/N .* ones(N, 1)
+            q_aux_list = q_aux_list[idx]
+            res[k] = 1
+        else
+            Xr = X[:, :, k-1]
+            Wr = W[:, k-1]
         end
 
-        # Resample
-        for i = 1:N
-            Xr[i, :] = X[idx[i], :, k-1]
-        end
-        q_aux_list = q_aux_list[idx]
 
         # Propagate
         if Γ[k] == 1
@@ -198,7 +223,7 @@ function eapf(y, sys, par, δ)
                 Σ = fix_sym(Σ)
 
                 q_list[i] = MvNormal(μ[1] + Σ[1,2]*inv(Σ[2,2])*(y[:,k] - μ[2]),
-                                    Σ[1,1] - Σ[1,2]*inv(Σ[2,2])*Σ[1,2]')
+                                     sqrt(Σ[1,1] - Σ[1,2]*inv(Σ[2,2])*Σ[1,2]'))
                 X[i, :, k] = rand(q_list[i])
             end
         else
@@ -207,11 +232,22 @@ function eapf(y, sys, par, δ)
                 Σ = JP_sig(Xr[i, :], k, R + Vn)
                 Σ = fix_sym(Σ)
 
-                μ_func(yh) = μ[1] + Σ[1,2]*inv(Σ[2,2])*(yh - μ[2])
-                #Σ = Σ[1,1] - Σ[1,2]*inv(Σ[2,2])*Σ[1,2]'
+                μ_func(x) = μ[1] + Σ[1,2]*inv(Σ[2,2])*(x - μ[2])
+                S = sqrt(Σ[1,1] - Σ[1,2]*inv(Σ[2,2])*Σ[1,2])
 
-                MD = MixtureModel(map(y -> MvNormal([μ_func(y)...],
-                        Σ[1,1] - Σ[1,2]*inv(Σ[2,2])*Σ[1,2]), yh))
+                wh = zeros(M)
+                for j = 1:M
+                    wh[j] = pdf(MvNormal(μ[2], Σ[2, 2]), yh[j, :])
+                end
+
+                if sum(wh) > 0
+                    wh = wh ./ sum(wh)
+                else
+                    println("Bad conditioned weights for Mixture Gaussian; resetting to uniform")
+                    wh = 1 / M * ones(M)
+                end
+
+                MD = MixtureModel(map(y -> MvNormal([μ_func(y)...], S), yh), wh)
 
                 q_list[i] = MD
                 X[i, :, k] = rand(q_list[i])
@@ -221,28 +257,42 @@ function eapf(y, sys, par, δ)
         # Weight
         if Γ[k] == 1
             for i = 1:par.N
-                W[i, k] = pdf(v, Z[:,k] - h(X[i, :, k], k))[1] *
-                          pdf(w, X[i, :, k] - f(Xr[i, :], k))[1] /
-                          (pdf(q_list[i], X[i, :, k]) * pdf(q_aux_list[i], Z[:, k]))
+                if res[k] == 1
+                    W[i, k] = Wr[i]*pdf(v, Z[:,k] - h(X[i, :, k], k))[1] *
+                            pdf(w, X[i, :, k] - f(Xr[i, :], k))[1] /
+                            (pdf(q_list[i], X[i, :, k]) * pdf(q_aux_list[i], Z[:, k]))
+                else
+                    W[i, k] = Wr[i]*pdf(v, Z[:,k] - h(X[i, :, k], k))[1] *
+                            pdf(w, X[i, :, k] - f(Xr[i, :], k))[1] /
+                            (pdf(q_list[i], X[i, :, k]))
+                end
             end
         else
             for i = 1:par.N
                 # Likelihood
-                D = Normal(h(X[i, :, k], k)[1], R)
+                D = Normal(h(X[i, :, k], k)[1], sqrt(R))
                 py = cdf(D, Z[k] + δ) - cdf(D, Z[k] - δ)
+
 
                 # Propagation
                 px = pdf(w, X[i, :, k] - f(Xr[i, :], k))[1]
 
-                # Predictive likelihood
-                pL = 0
-                for j = 1:M
-                    pL += pdf(q_aux_list[i], yh[j, :])
-                end
                 # proposal distribution
                 q = pdf(q_list[i], X[i, :, k])
+                
+                if res[k] == 1
+                    # Predictive likelihood
+                    pL = 0
+                    for j = 1:M
+                        pL += pdf(q_aux_list[i], yh[j, :])
+                    end
+                    
 
-                W[i, k] = py*px / (pL*q)
+                    W[i, k] = Wr[i]*py*px / (pL*q)
+                else
+                    W[i, k] = Wr[i]*py*px / q
+                end
+
 
             end
         end
@@ -253,93 +303,12 @@ function eapf(y, sys, par, δ)
         else
             println("Bad conditioned weights for EAPF! Resetting to uniform")
             W[:, k] = 1/par.N * ones(par.N, 1)
+            fail[k] = 1
+            Neff[k] = 0
         end
 
 
     end
 
-    return X, W, Z, Γ
-end
-
-"""
-Event-based state estimator
-"""
-function ebse(y, sys, δ)
-
-    # Extract parameters
-    A = sys.A
-    C = sys.C
-    Q = sys.Q
-    R = sys.R
-
-    T = sys.T
-
-    # === For approximating the uniform distribution
-    # number of approximation points and their spacing
-    M = 20 #ceil(2 * δ) + 1
-    L = 2*δ / (M-1)
-
-    Vn = L / sqrt(2)
-    # ===
-
-    nx = size(A, 1)
-    ny = size(C, 1)
-
-    xp = zeros(nx, T)
-    Pp = zeros(nx, nx, T)
-    yp = zeros(ny, T)
-    Z = zeros(ny, T)
-    Γ = zeros(T)
-
-    x = zeros(nx, T)
-    P = zeros(nx, nx, T)
-
-    P[:,:,1] = eye(nx)
-
-    for k = 2:T
-
-        # Run event kernel, SOD
-        if norm(Z[:, k-1] - y[:, k]) >= δ
-            Γ[k] = 1
-            Z[:, k] = y[:, k]
-        else
-            Γ[k] = 0
-            Z[:, k] = Z[:, k-1]
-
-            # Discretisize the uniform distribution, currently only supports
-            # dim(y) = 1
-            yh = vcat(linspace(Z[:, k]- δ, Z[:, k] + δ, M)...)
-        end
-
-        # Predict
-        xp = A*x[:, k-1]
-        Pp = A*P[:, :, k-1]*A' + Q
-
-        # Update
-        S = inv(inv(Pp) + C'*inv(R)*C)
-
-        if Γ[k] == 1
-            x[:, k] = S*(inv(Pp)*xp + C'*inv(R)*Z[:, k])
-            P[:, :, k] = S
-        else
-            θ = zeros(2, M)
-            w = zeros(1, M)
-            for i = 1:M
-                θ[:, i] = S*(inv(Pp)*xp + C'*inv(R)*yh[i])
-                w[:, i] = pdf(MvNormal(C*xp, C*Pp*C' + R + Vn), yh[i, :])
-            end
-            w /= sum(w)
-
-            for i = 1:M
-                x[:, k] += w[:, i] .* θ[:, i]
-            end
-
-            for i = 1:M
-                P[:, :, k] += w[:, i] .* (S + (x[:, k] - θ[:, i])*(x[:, k] - θ[:, i])')
-            end
-        end
-    end
-
-    return x, P, Z, Γ
-
+    return X, W, Z, Γ, Neff, res, fail
 end
