1e11298ccae73c4c859b032ec7173ab28867e2e3
Johan Ruuskanen <johan.ruuskanen@gmail.com>
2019-02-08 22:54:02 +0100

    Refactoring: Working JUlia package structure

--- src/EventBasedParticleFiltering.jl
+++ src/EventBasedParticleFiltering.jl
@@ -1,3 +1,16 @@
 module EventBasedParticleFiltering
 
+    using JLD
+    using PyPlot
+    using Random
+    using StatsBase
+    using Distributions
+    using LinearAlgebra
+
+    include("linear_systems/filters.jl")
+    include("linear_systems/filters_eventbased.jl")
+
+    include("funcs/misc.jl")
+    include("funcs/plotting.jl")
+
 end
