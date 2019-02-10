68a06fbd66781e52198df0f07e6ad609b69315b4
Johan Ruuskanen <johan.ruuskanen@gmail.com>
2019-02-10 20:02:45 +0100

    Refactoring: Split the linear_systems folder into futureWork and current functions

--- src/EventBasedParticleFiltering.jl
+++ src/EventBasedParticleFiltering.jl
@@ -24,9 +24,8 @@ module EventBasedParticleFiltering
     using Distributions
     using LinearAlgebra
 
-    include("linear_systems/filters.jl")
-    include("linear_systems/filters_eventbased.jl")
-
+    include("funcs/filters.jl")
+    include("funcs/filters_eventbased.jl")
     include("funcs/misc.jl")
     include("funcs/plotting.jl")
 
