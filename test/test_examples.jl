896907184a5f047bd8b6333d695736cf3e66b9ce
Johan Ruuskanen <johan.ruuskanen@control.lth.se>
2019-02-21 15:42:53 +0100

    Bugfix: Add more general path generation

--- test/test_examples.jl
+++ test/test_examples.jl
@@ -1,11 +1,12 @@
 @testset "test_examples" begin
 
-example_path = "../examples/"
+current_path = @__DIR__
+example_path = current_path[1:end-4] * "examples/"
 scripts = readdir(example_path)
 
 for script in scripts
     println("Testing "*script)
-    @test test_script_crash(example_path * script)    
+    @test test_script_crash(example_path * script)
 end
 
 end
