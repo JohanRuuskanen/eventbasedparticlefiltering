360aad16792b35b8b7bd7e0d6930c1009b535138
Johan Ruuskanen <johan.ruuskanen@gmail.com>
2019-02-22 22:28:47 +0100

    Testing: Add a few more tests to propagation

--- test/test_propagation.jl
+++ test/test_propagation.jl
@@ -18,4 +18,32 @@
     @test any(x-> x == true, err_mean .< 0.15)
     @test any(x-> x == true, err_var .< 0.15)
 
+    N = 200
+    M = 10
+    T = 1000
+    δ = 4
+
+    A = reshape([1.0], 1, 1)
+    C = reshape([1.0], 1, 1)
+
+    nx = size(A, 1)
+    ny = size(C, 1)
+
+    Q = 1*Matrix{Float64}(I, nx, nx)
+    R = 0.1*Matrix{Float64}(I, ny, ny)
+
+    sys = sys_params(A, C, Q, R, T)
+    x, y = sim_sys(sys)
+
+    y = [0.5]; z = [1.0]; δ = 2
+    Vn = 0.1; γ = 0
+    yh = vcat(range(z .- δ, stop=(z .+ δ), length=M)...)
+    Xr = randn(N, 1)
+
+    X, q_list = propagation_locallyOptimal(Xr, z, sys, yh, Vn, γ)
+
+    @test size(q_list) == (N,)
+    @test size(X) == (N, nx)
+    @test mean(X) > mean(Xr)
+
 end
