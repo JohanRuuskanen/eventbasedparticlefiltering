f728636558879d57dbe57324c50f18ec2f5abb26
Johan Ruuskanen <johan.ruuskanen@control.lth.se>
2019-02-25 14:54:45 +0100

    Refactoring: Add comments to resampling tests

--- test/test_resampling.jl
+++ test/test_resampling.jl
@@ -23,17 +23,20 @@
     W = ones(N, 2) ./ N
 
     idx = EP.resampling_systematic(W[:, 1])
+    # Check that the size and type are correct
     @test size(idx, 1) == N
     @test typeof(idx) == Array{Int64, 1}
 
     idx = EP.resampling_multinomial(W[:, 1])
+    # Check that the size and type are correct
     @test size(idx, 1) == N
     @test typeof(idx) == Array{Int64, 1}
 
+    # When resampling with uniform weights, the resampled indexes should be
+    # roughly uniform, represented here as a deviation of maximum 15% from a
+    # uniform indexing
     diff_systematic = sampling_uniform(N, M, EP.resampling_systematic)
     diff_multinomial = sampling_uniform(N, M, EP.resampling_multinomial)
-
-    # Less than 15% off for each value
     @test (diff_systematic .< M * 0.15) == BitArray{1}(ones(N))
     @test (diff_multinomial .< M * 0.15) == BitArray{1}(ones(N))
 
