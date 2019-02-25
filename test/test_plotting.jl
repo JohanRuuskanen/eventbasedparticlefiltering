95dc1fe430a1a0003fef8f5bbbf291dabd1c0ab5
Johan Ruuskanen <johan.ruuskanen@control.lth.se>
2019-02-25 14:40:18 +0100

    Refactioring: Add comments to the plotting tests

--- test/test_plotting.jl
+++ test/test_plotting.jl
@@ -1,5 +1,6 @@
 @testset "test_plotting" begin
 
+# Create som random, dummy system
 Random.seed!(123)
 
 m = 10; n = 50;
@@ -7,10 +8,9 @@ X = Random.rand(m, n); W = Random.rand(m, n); S = Random.rand(m, n);
 x_true = Random.rand(1, n); y = Random.rand(n); z = Random.rand(n)
 Γ = Random.rand([0, 1], n); δ = 10
 
+# Test that the different plot function does not crash
 @test try plot_particle_trace(X, S, x_true=x_true, Γ=Γ); true catch; false end
-
 @test try plot_data(y, z, δ=δ); true catch; false end
-
 @test try plot_effective_sample_size(W; Γ=Γ); true catch; false end
 
 end
