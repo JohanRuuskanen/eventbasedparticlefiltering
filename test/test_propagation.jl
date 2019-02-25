6758dc1d33a05d5ae9c18b1841710308c70e79f6
Johan Ruuskanen <johan.ruuskanen@control.lth.se>
2019-02-25 14:45:54 +0100

    Refactoring: Add comments to propagation test

--- test/test_propagation.jl
+++ test/test_propagation.jl
@@ -13,14 +13,15 @@
 
     err_mean = abs.(mean(X_new, dims=1))
     err_var = abs.(var(X_new, dims=1)) .- 1
-    
+
+    # Test that the error after propagation is smaller than some small dummy
+    # value
     @test any(x-> x == true, err_mean .< 0.15)
     @test any(x-> x == true, err_var .< 0.15)
 
-    N = 200
-    M = 10
-    T = 1000
-    δ = 4
+    # Test the locally optimal propagation. Current implementation only supports
+    # ny = 1, thus we need to redefine the system
+    N = 200; M = 10; T = 1000; δ = 4
 
     A = reshape([1.0], 1, 1)
     C = reshape([1.0], 1, 1)
@@ -41,8 +42,11 @@
 
     X, q_list = propagation_locallyOptimal(Xr, z, sys, yh, Vn, γ)
 
+    # Check that the returned values are of the correct sizes
     @test size(q_list) == (N,)
     @test size(X) == (N, nx)
+
+    # The mean should have increased, moved from ≈ 0 towards 1.0
     @test mean(X) > mean(Xr)
 
 end
