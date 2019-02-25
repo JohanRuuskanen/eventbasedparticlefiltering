43cc4830a8600e14f9d60d020a6873401ebef863
Johan Ruuskanen <johan.ruuskanen@control.lth.se>
2019-02-25 15:01:56 +0100

    Refactoring: Add comments to weight calculation tests

--- test/test_weighting.jl
+++ test/test_weighting.jl
@@ -1,5 +1,31 @@
 @testset "test_weighting" begin
 
+    function local_tests(r, γ)
+
+        println("With r:$(r), γ:$(γ)")
+
+        W = zeros(N)
+        X = randn(N, nx)
+        z = reshape([1.0], 1)
+        Wr = ones(N) ./ N
+        Xr = randn(N, nx)
+        yh = vcat(range(z .- δ, stop=(z .+ δ), length=M)...)
+
+        q_list = Array{Distribution}(undef, N)
+        q_aux_list = Array{Distribution}(undef, N)
+        for k = 1:N
+            q_list[k] = MvNormal(zeros(nx), Matrix{Float64}(I, nx, nx))
+            q_aux_list[k] = MvNormal(zeros(ny), Matrix{Float64}(I, ny, ny))
+        end
+
+
+        calculate_weights!(W, X, z, Wr, Xr, yh, q_list, q_aux_list, r, γ, δ, sys, par)
+
+        # Check that the calculated weights have the correct size and type
+        @test size(W) == (N,)
+        @test typeof(W) == Array{Float64,1}
+    end
+
     nx = 10; ny = 1
     N = 1000; M = 20
     δ = 4
@@ -15,38 +41,18 @@
 
     par = pf_params(N, "SOD", N/2)
 
-    W = zeros(N)
-    X = randn(N, nx)
-    z = reshape([1.0], 1)
-    Wr = ones(N) ./ N
-    Xr = randn(N, nx)
-    yh = vcat(range(z .- δ, stop=(z .+ δ), length=M)...)
-
-    q_list = Array{Distribution}(undef, N)
-    q_aux_list = Array{Distribution}(undef, N)
-    for k = 1:N
-        q_list[k] = MvNormal(zeros(nx), Matrix{Float64}(I, nx, nx))
-        q_aux_list[k] = MvNormal(zeros(ny), Matrix{Float64}(I, ny, ny))
-    end
-
+    # Test the weight calculation functions with/without resampling and
+    # with/without triggered event
     r = 0; γ = 0
-    calculate_weights!(W, X, z, Wr, Xr, yh, q_list, q_aux_list, r, γ, δ, sys, par)
-    @test size(W) == (N,)
-    @test typeof(W) == Array{Float64,1}
+    local_tests(r, γ)
 
     r = 0; γ = 1
-    calculate_weights!(W, X, z, Wr, Xr, yh, q_list, q_aux_list, r, γ, δ, sys, par)
-    @test size(W) == (N,)
-    @test typeof(W) == Array{Float64,1}
+    local_tests(r, γ)
 
     r = 1; γ = 0
-    calculate_weights!(W, X, z, Wr, Xr, yh, q_list, q_aux_list, r, γ, δ, sys, par)
-    @test size(W) == (N,)
-    @test typeof(W) == Array{Float64,1}
+    local_tests(r, γ)
 
     r = 1; γ = 1
-    calculate_weights!(W, X, z, Wr, Xr, yh, q_list, q_aux_list, r, γ, δ, sys, par)
-    @test size(W) == (N,)
-    @test typeof(W) == Array{Float64,1}
+    local_tests(r, γ)
 
 end
